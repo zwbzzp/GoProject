b82072fabd8e49c257cf45f6175faa4c3f07dbe4
zhuwenbiao <zwbzzp@qq.com>
2018-05-21 01:23:27 +0800

    first commit

---
+++ src/GoProject/FileServer/fs.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"log"
+)
+
+func main() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+	log.Printf("Serve %s on Http port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":" + *port, nil))
+}
